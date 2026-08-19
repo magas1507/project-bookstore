be23d94c09ca490e40819e5b1a90e65d1daf9b35
magas1507 <asmg15.07@gmail.com>
2026-08-19 19:04:00 -0300

    feat(book): Implementing validations for clientService, specifically the findById, update and delete

--- src/services/ClientService.ts
+++ src/services/ClientService.ts
@@ -29,4 +29,44 @@ export class ClientService {
   public async findAll(): Promise<Client[]> {
     return this.clientRepository.findAll();
   }
+
+
+  public async findById(id: number): Promise<Client> {
+    const client = await this.clientRepository.findById(id);
+    if (!client) throw new Error(`Cliente com ID ${id} não encontrado.`);
+    return client;
+  }
+
+  public async update(id: number, name: string, email?: string, phone?: string): Promise<Client> {
+    await this.findById(id);
+
+    const client: Client = {
+      name: name.trim()
+    };
+
+    if (email) {
+      client.email = email.trim();
+    }
+
+    if (phone) {
+      client.phone = phone.trim();
+    }
+
+    const updated = await this.clientRepository.update(id, client);
+    if (!updated) throw new Error('Erro ao atualizar cliente.');
+    return updated;
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this.findById(id);
+    try {
+      const deleted = await this.clientRepository.delete(id);
+      if (!deleted) throw new Error('Erro ao remover cliente.');
+    } catch (error: any) {
+      if (error.code === '23503') {
+        throw new Error('Não é possível remover: cliente possui empréstimos.');
+      }
+      throw error;
+    }
+  }
 }  
